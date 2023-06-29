f9edc52934f663be9e9bc2825692995486caa871
Abdelrahman Ehab <abdu.ehab0@gmail.com>
2023-06-29 20:26:08 +0300

    updated readme

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 A differential equation solver using the Euler-Method. The current example simulates a spring-mass-damper system. The PID-Controller example has been implemented. The PID-Controller can be tuned using the Ziegler-Nichols method.
 
 ## Outputs
+Free Response:
+![Free Response](spring_mass_damper_free_response.png)
 
-<!-- ![Demo 1](https://https://github.com/AbduEhab/Rust-Defferential-Equation-Solver/blob/main/render.jpg) -->
-![Alt text](spring_mass_damper_free_response.png)
+PID Response:
+![PID Response](spring_mass_damper_manual_pid_response.png)
+
+Tuned PID Response:
+![Tuned PID Response](spring_mass_damper_pid_tuned_response.png)
+
+## Usage
+Replace the equation in the main forloop at line 45 with your differential equation. The tuning will be done automatically. The PID-Controller can be tuned manually by changing the values in the PID-Controller at line 18-20. Setting the values to 0 will disable the PID-Controller, setting the "no_tuner" variable with disable the tuner.
+
 
 ## Features
 - [x] Euler-Method
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ndarray::Array1;
 use plotters::prelude::*;
 
+const TUNER: bool = false;
+
 fn main() {
     // Define the parameters for the spring-mass-damper system
     let m: f64 = 1.0; // mass
@@ -8,20 +10,20 @@ fn main() {
     let k: f64 = 1.0; // spring constant
 
     // Set initial conditions
-    let x0: f64 = 0.0; // initial position
-    let v0: f64 = 1.0; // initial velocity
+    let x0: f64 = 0.; // initial position
+    let v0: f64 = 1.; // initial velocity
 
     // Set the desired position
     let x_desired: f64 = 1.;
 
     // PID controller gains
-    let mut kp: f64 = 0.0; // Proportional gain
-    let mut ki: f64 = 0.0; // Integral gain
-    let mut kd: f64 = 0.0; // Derivative gain
+    let mut kp: f64 = 1.0; // Proportional gain
+    let mut ki: f64 = 0.5; // Integral gain
+    let mut kd: f64 = 0.9; // Derivative gain
 
     // Define the range of values for t (time)
-    let t_start: f64 = 0.0;
-    let t_end: f64 = 100.0;
+    let t_start: f64 = 0.;
+    let t_end: f64 = 100.;
     let h: f64 = 0.01; // Step size
 
     // Calculate the number of steps
@@ -38,8 +40,8 @@ fn main() {
     v_values[0] = v0;
 
     // Initialize the integral and derivative terms of the PID controller
-    let mut integral_term = 0.0;
-    let mut prev_error = 0.0;
+    let mut integral_term = 0.;
+    let mut prev_error = 0.;
 
     // Perform numerical integration using Euler's method
     for i in 0..num_steps {
@@ -73,8 +75,7 @@ fn main() {
     }
 
     // Plot the solution trajectory
-    let root =
-        BitMapBackend::new("spring_mass_damper_pid_tuned.png", (800, 600)).into_drawing_area();
+    let root = BitMapBackend::new("spring_mass_damper.png", (800, 600)).into_drawing_area();
     root.fill(&WHITE).unwrap();
 
     let t_min = *t_values
@@ -114,58 +115,60 @@ fn main() {
         ))
         .unwrap();
 
-    // Perform Tyreus-Luyben PID tuning
-    let tuning_duration = 5.0; // Duration for step testing
-    let tuning_step = 0.1; // Step size for tuning
+    if TUNER {
+        // Perform Tyreus-Luyben PID tuning
+        let tuning_duration = 5.0; // Duration for step testing
+        let tuning_step = 0.1; // Step size for tuning
 
-    let mut integral_sum = 0.0;
-    let mut prev_tuning_error = 0.0;
+        let mut integral_sum = 0.0;
+        let mut prev_tuning_error = 0.0;
 
-    for _ in 0..((tuning_duration / tuning_step) as usize) {
-        let tuning_error = x_desired - x_values[x_values.len() - 1];
-        integral_sum += tuning_error * tuning_step;
-        let derivative = (tuning_error - prev_tuning_error) / tuning_step;
+        for _ in 0..((tuning_duration / tuning_step) as usize) {
+            let tuning_error = x_desired - x_values[x_values.len() - 1];
+            integral_sum += tuning_error * tuning_step;
+            let derivative = (tuning_error - prev_tuning_error) / tuning_step;
 
-        kp = 0.6 / tuning_error;
-        ki = 1.2 / integral_sum;
-        kd = 0.075 / derivative;
+            kp = 0.6 / tuning_error;
+            ki = 1.2 / integral_sum;
+            kd = 0.075 / derivative;
 
-        for i in 0..num_steps {
-            let t = t_values[i];
-            let x = x_values[i];
-            let v = v_values[i];
+            for i in 0..num_steps {
+                let t = t_values[i];
+                let x = x_values[i];
+                let v = v_values[i];
 
-            let error = x_desired - x;
-            let control_input = kp * error + ki * integral_term + kd * (error - prev_error) / h;
+                let error = x_desired - x;
+                let control_input = kp * error + ki * integral_term + kd * (error - prev_error) / h;
 
-            integral_term += error * h;
+                integral_term += error * h;
 
-            let dx_dt = v;
-            let dv_dt = (-c / m) * v - (k / m) * x + control_input;
+                let dx_dt = v;
+                let dv_dt = (-c / m) * v - (k / m) * x + control_input;
 
-            let dt = h;
+                let dt = h;
 
-            let x_next = x + dx_dt * dt;
-            let v_next = v + dv_dt * dt;
-            let t_next = t + dt;
+                let x_next = x + dx_dt * dt;
+                let v_next = v + dv_dt * dt;
+                let t_next = t + dt;
 
-            t_values[i + 1] = t_next;
-            x_values[i + 1] = x_next;
-            v_values[i + 1] = v_next;
+                t_values[i + 1] = t_next;
+                x_values[i + 1] = x_next;
+                v_values[i + 1] = v_next;
 
-            prev_error = error;
+                prev_error = error;
+            }
+
+            prev_tuning_error = tuning_error;
         }
 
-        prev_tuning_error = tuning_error;
+        // Plot the system response after tuning
+        chart
+            .draw_series(LineSeries::new(
+                t_values.iter().zip(x_values.iter()).map(|(&t, &x)| (t, x)),
+                &RED,
+            ))
+            .unwrap();
     }
-
-    // Plot the system response after tuning
-    chart
-        .draw_series(LineSeries::new(
-            t_values.iter().zip(x_values.iter()).map(|(&t, &x)| (t, x)),
-            &RED,
-        ))
-        .unwrap();
 
     // print the final values of the PID gains
     println!("kp = {}, ki = {}, kd = {}", kp, ki, kd);
